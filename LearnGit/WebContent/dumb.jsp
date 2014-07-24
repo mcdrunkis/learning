7fa7e5f4971ad548c4f97e372fdaae8ea0224cc1
Dan <dan.rempe@gmail.com>
2014-07-24 13:55:55 -0600

    Created a dumb servlet.

---
+++ LearnGit/WebContent/dumb.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<%
+	String dumb = null;
+	if (request.getAttribute("DUMB") != null) {
+		dumb = (String)request.getAttribute("DUMB");
+	}
+%>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>A dumb page</title>
+</head>
+<body>
+	<%= dumb != null ? dumb : "" %>
+</body>
+</html>
--- LearnGit/src/com/dan/test/SomeDumbServlet.java
+++ LearnGit/src/com/dan/test/SomeDumbServlet.java
@@ -1,7 +1,27 @@
 package com.dan.test;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/dumb")
 public class SomeDumbServlet extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setAttribute("DUMB","I am the dumbest servlet ever.");
+		request.getRequestDispatcher("/dumb.jsp").forward(request,response);
+	}
 }
